9f9689ba0c8bcfc22ee7529e19bae123fae2f1be
Sameena-20 <sameenaichu@gmail.com>
2025-06-23 19:20:20 +0530

    updated version of sum

--- sample.py
+++ sample.py
@@ -1,7 +1,11 @@
 n=int(input("Enter the number:"))
 s=0
+r=0
 while(n>0):
     d=n%10
     s=s+d
+    r=r*10+d
     n=n//10
 print("Sum of digits:",s)
+print("________Updated part of the code_______")
+print("Reverse the number:",r)
